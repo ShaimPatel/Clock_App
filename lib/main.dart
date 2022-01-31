40621c7f0ea78d19b688d9a4845749486e6d2dca
ShaimPatel <sp5036018@gmail.com>
2022-01-31 20:06:04 +0530

    After Changes

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,17 @@ import 'package:clock_app/screen/ragistration_page.dart';
 import 'package:clock_app/screen/signin_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  FlutterNativeSplash.removeAfter(initialization);
   runApp(MyApp());
+}
+
+Future initialization(BuildContext? context) async {
+  await Future.delayed(const Duration(seconds: 1));
 }
 
 class MyApp extends StatelessWidget {
