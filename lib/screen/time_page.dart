1c58faeb4fcd2d73ae38fb082e9a9eb39e05461e
ShaimPatel <sp5036018@gmail.com>
2022-02-03 17:47:51 +0530

    Digital Clock Change

--- lib/screen/time_page.dart
+++ lib/screen/time_page.dart
@@ -5,6 +5,7 @@ import 'dart:async';
 import 'package:custom_switch/custom_switch.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:slide_digital_clock/slide_digital_clock.dart';
 
 class TimePage extends StatefulWidget {
   const TimePage({Key? key}) : super(key: key);
@@ -18,7 +19,8 @@ class _TimePageState extends State<TimePage> {
   late int h;
   late int m;
   late int s;
-  bool status = true;
+  // bool status = false;
+  bool isVisible = false;
 
   @override
   void initState() {
@@ -52,66 +54,100 @@ class _TimePageState extends State<TimePage> {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: Row(
+      child: Column(
         children: [
-          status
-              ? Expanded(
-                  flex: 3,
-                  child: Row(
-                    children: [
-                      Expanded(
-                        flex: 1,
-                        child: Text(
-                            '${(H < 10) ? "0$H" : H}:${(m < 10) ? "0$m" : m}:${(s < 10) ? "0$s" : s}',
-                            style: const TextStyle(
-                              fontSize: 32.0,
-                            )),
-                      ),
-                      // Expanded(
-                      //   flex: 1,
-                      //   child: Text(
-                      //     (H >= 12) ? "P.M" : "A.M",
-                      //     style: const TextStyle(fontSize: 25),
-                      //   ),
-                      // )
-                    ],
-                  ))
-              : Expanded(
-                  flex: 3,
-                  child: Row(
-                    children: [
-                      Expanded(
-                        flex: 1,
-                        child: Text(
-                          '${(h < 10) ? "0$h" : h}:${(m < 10) ? "0$m" : m}:${(s < 10) ? "0$s" : s}',
-                          style: const TextStyle(
-                            fontSize: 32.0,
-                          ),
-                        ),
-                      ),
-                      Expanded(
-                        flex: 1,
-                        child: Text(
-                          (H >= 12) ? "P.M" : "A.M",
-                          style: const TextStyle(fontSize: 15),
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-          Expanded(
-            flex: 1,
-            child: CustomSwitch(
-              activeColor: Colors.green,
-              value: status,
-              onChanged: (v) {
-                // print("V : $v");
-                setState(() {
-                  status = v;
-                  // Fluttertoast.showToast(msg: '$status');
-                });
-              },
+          // status
+          //     ? Expanded(
+          //         flex: 3,
+          //         child: Row(
+          //           children: [
+          //             Expanded(
+          //               flex: 1,
+          //               child: Text(
+          //                   '${(H < 10) ? "0$H" : H}:${(m < 10) ? "0$m" : m}:${(s < 10) ? "0$s" : s}',
+          //                   style: const TextStyle(
+          //                     fontSize: 32.0,
+          //                   )),
+          //             ),
+          //             // Expanded(
+          //             //   flex: 1,
+          //             //   child: Text(
+          //             //     (H >= 12) ? "P.M" : "A.M",
+          //             //     style: const TextStyle(fontSize: 25),
+          //             //   ),
+          //             // )
+          //           ],
+          //         ))
+          //     : Expanded(
+          //         flex: 3,
+          //         child: Row(
+          //           children: [
+          //             Expanded(
+          //               flex: 1,
+          //               child: Text(
+          //                 '${(h < 10) ? "0$h" : h}:${(m < 10) ? "0$m" : m}:${(s < 10) ? "0$s" : s}',
+          //                 style: const TextStyle(
+          //                   fontSize: 32.0,
+          //                 ),
+          //               ),
+          //             ),
+          //             Expanded(
+          //               flex: 1,
+          //               child: Text(
+          //                 (H >= 12) ? "P.M" : "A.M",
+          //                 style: const TextStyle(fontSize: 15),
+          //               ),
+          //             )
+          //           ],
+          //         ),
+          //       ),
+          Visibility(
+            visible: isVisible,
+            child: DigitalClock(
+              digitAnimationStyle: Curves.elasticOut,
+              is24HourTimeFormat: true,
+              areaDecoration: const BoxDecoration(
+                color: Colors.transparent,
+              ),
+              hourMinuteDigitTextStyle: const TextStyle(
+                color: Colors.blueGrey,
+                fontSize: 50,
+              ),
+              amPmDigitTextStyle: const TextStyle(
+                  color: Colors.blueGrey, fontWeight: FontWeight.bold),
             ),
+          ),
+          Visibility(
+            visible: !isVisible,
+            child: DigitalClock(
+              digitAnimationStyle: Curves.elasticOut,
+              is24HourTimeFormat: false,
+              areaDecoration: const BoxDecoration(
+                color: Colors.transparent,
+              ),
+              hourMinuteDigitTextStyle: const TextStyle(
+                color: Colors.blueGrey,
+                fontSize: 50,
+              ),
+              amPmDigitTextStyle: const TextStyle(
+                  color: Colors.blueGrey, fontWeight: FontWeight.bold),
+            ),
+          ),
+
+          const SizedBox(
+            height: 25,
+          ),
+          CustomSwitch(
+            activeColor: Colors.green,
+            value: isVisible,
+            onChanged: (v) {
+              // print("V : $v");
+              setState(() {
+                isVisible = !isVisible;
+                // status = v;
+                // Fluttertoast.showToast(msg: '$status');
+              });
+            },
           ),
         ],
       ),
